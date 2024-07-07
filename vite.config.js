f51926ab9c592d8566b2e08a8d9edca7354b7923
ashishh06 <ashishhariom14@gmail.com>
2024-07-08 00:58:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-  'process.env':{}
+  'process.env':{
+    VITE_API_KEY: JSON.stringify(process.env.VITE_API_KEY),
+    NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+  }
   }
 })
